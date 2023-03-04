5b431407cc81e9226518a255377e50f420b3cbcc
windrider <1700013038@pku.edu.cn>
2023-03-04 23:47:19 +0800

    second push

--- README.md
+++ README.md
@@ -84,7 +84,7 @@ void pageRank(Graph g, double* solution, double damping, double convergence)
 
 ​	int index=new_frontier->count++;这一句如果被所有线程不加限制地异步计算会导致count计数发生错误。所以需要加上#pragma omp critical限制同时只能有一条指令执行该语句。
 
-​	最后修改的代码如下：
+​	最后修改的代码如下（frontier为入参，new_frontier为出参，每轮从frontier找邻接节点放入new_frontier）：
 
 ```c++
 void top_down_step(
@@ -113,6 +113,7 @@ void top_down_step(
                     #pragma omp critical
                     index=new_frontier->count++;
                     new_frontier->vertices[index] = outgoing;
+                    
                 }
             }
         //}
@@ -128,12 +129,163 @@ void top_down_step(
 
 ![9bb1d132f4907fc79dceb247efde6de](graphs/9bb1d132f4907fc79dceb247efde6de.png)
 
-可以看到从单线程到双线程性能提高了，但是到4个线程时并没有让程序更快。这是因为**#pragma omp critical**不可避免地会造成线程阻塞，线程数量增加，阻塞的次数也会增加。如果能只用原子操作就解决共享变量的访问问题，程序性能应该还能提高。
+可以看到从单线程到双线程性能提高了，但是到4个线程时并没有让程序更快。这是因为**#pragma omp critical**不可避免地会造成线程阻塞，线程数量增加，阻塞的次数也会增加。
 
 
 
-​	另外，该函数有两层循环，实验中尝试过只将**pragma omp parallel**加到内层循环上，发现程序执行得很慢，并且线程越多会越慢
+尝试使用CAS实现无锁：
+
+```c++
+void top_down_step(
+    Graph g,
+    vertex_set* frontier,
+    vertex_set* new_frontier,
+    int* distances)
+{
+    #pragma omp parallel for
+    for (int i=0; i<frontier->count; i++) {
+        int node = frontier->vertices[i];
+        int start_edge = g->outgoing_starts[node];
+        int end_edge = (node == g->num_nodes - 1)
+                           ? g->num_edges
+                           : g->outgoing_starts[node + 1];
+            for (int neighbor=start_edge; neighbor<end_edge; neighbor++) {
+                int outgoing = g->outgoing_edges[neighbor];
+                if (distances[outgoing] == NOT_VISITED_MARKER) {
+                    distances[outgoing] = distances[node] + 1;
+                    while(!__sync_bool_compare_and_swap(new_frontier->vertices+new_frontier->count,NEWFRONTIER_OLD,outgoing));
+                    #pragma omp atomic
+                    new_frontier->count++;
+                }
+            }
+    }
+}
+```
+
+程序运行效率有了一些微小的提高：
+
+![1454c592e9075d6c41d299479735bb0](graphs/1454c592e9075d6c41d299479735bb0.png)
+
+ 
+
+ 后续更新：CAS那里直接写成
+
+```c++
+while(!__sync_bool_compare_and_swap(new_frontier->vertices+new_frontier->count++,NEWFRONTIER_OLD,outgoing));
+```
+
+可以提升到这个程度
+
+​	![56bd5f7dd3e732522df7cdb70a14fe8](graphs/56bd5f7dd3e732522df7cdb70a14fe8.png)
+
+
+
+另外，该函数有两层循环，实验中尝试过只将**pragma omp parallel**加到内层循环上，发现程序执行得很慢，并且线程越多会越慢
 
 ![648eda5628e3a6db52222dbd10bf9be](graphs/648eda5628e3a6db52222dbd10bf9be.png)
 
 ​	这是因为内层循环体只有很简短的计算和赋值，导致线程切换得很快，线程切换也会有开销，并且会有更频繁地阻塞。而且这样的设计会让线程之间频繁访问不连续的内存空间，弄脏cache。
+
+## Part 3: "Bottom Up" BFS
+
+要求实验者自行实现自底向上的BFS算法，并对其进行加速优化。
+
+在题目的文档中给出了算法的思路：
+
+```
+for each vertex v in graph:
+    if v has not been visited AND 
+       v shares an incoming edge with a vertex u on the frontier:
+          add vertex v to frontier;
+```
+
+​		一开始的想法是维护一个unvisited_set存储所有还没有访问过的节点，当节点一旦被访问过就从unvisited_set中去除（将这轮访问到的节点放入new_frontier,将没访问过的节点放入new_unvisited_set，最后交换unvisited_set和new_unvisited_set）。并且模仿上个part的操作，维护一个从frontier到new_frontier的过程。
+
+```c++
+void bottom_up_step(
+    Graph g,
+    vertex_set* frontier,
+    vertex_set* new_frontier,
+    vertex_set* unvisited_set,
+    vertex_set* new_unvisited_set,
+    int* distances)
+{
+    int mydistance=0;
+    #pragma omp parallel for
+    for (int i=0; i<unvisited_set->count; i++) {
+
+        int node = unvisited_set->vertices[i];
+
+        int start_edge = g->incoming_starts[node];
+        int end_edge = (node == g->num_nodes - 1)
+                           ? g->num_edges
+                           : g->incoming_starts[node + 1];
+        
+        bool into_new_frontier=false;
+        for(int neighbor=start_edge;neighbor<end_edge;neighbor++){
+            int incoming=g->incoming_edges[neighbor];
+            if(distances[incoming]!=NOT_VISITED_MARKER){
+                mydistance=distances[incoming]+1;
+                while(!__sync_bool_compare_and_swap(new_frontier->vertices+new_frontier->count++,NOT_INIT,node));
+                into_new_frontier=true;
+                break;
+            }
+        }
+        if(!into_new_frontier){
+            while(!__sync_bool_compare_and_swap(new_unvisited_set->vertices+new_unvisited_set->count++,NOT_INIT,node));
+        }
+    }
+    #pragma omp parallel for
+    for(int i=0;i<new_frontier->count;i++){
+        int node=new_frontier->vertices[i];
+        distances[node]=mydistance;
+    }
+}
+```
+
+​		但实验中发现，在更新new_frontier和new_unvisited_set的时候，都需要加上CAS原子操作限制线程的访问，导致性能很差。
+
+​		改进的方法是，因为每轮的frontier到ROOT_NODE的距离都是相同的，并且这个距离就等于进行的轮数，所以用一个**lastdistance**变量表示上一轮得到的frontier到ROOT_NODE的距离，当一个点的distance等于lastdistance就代表它在上一轮算出的frontier上。并且现在unvisited_set就表示除了ROOT_NODE外的所有节点，不再每轮进行更新：
+
+```c++
+void bottom_up_step(
+    Graph g,
+    vertex_set* frontier,
+    vertex_set* new_frontier,
+    vertex_set* unvisited_set,
+    vertex_set* new_unvisited_set,
+    int* distances,
+    int lastdistance,
+    bool& newadd
+    )
+{
+    int mydistance=0;
+    #pragma omp parallel for
+    for (int i=0; i<unvisited_set->count; i++) {
+
+        int node = unvisited_set->vertices[i];
+        if(distances[node]!=NOT_VISITED_MARKER)continue;
+        int start_edge = g->incoming_starts[node];
+        int end_edge = (node == g->num_nodes - 1)
+                           ? g->num_edges
+                           : g->incoming_starts[node + 1];
+        
+
+        for(int neighbor=start_edge;neighbor<end_edge;neighbor++){
+            int incoming=g->incoming_edges[neighbor];
+            if(distances[incoming]==lastdistance){                
+                distances[node]=lastdistance+1;
+                newadd=true;
+                break;
+            }
+        }
+    }
+    
+}
+```
+
+​		最后结果如下：
+
+
+
+![23adba3a2bb600020c4b3577dcac973](graphs/23adba3a2bb600020c4b3577dcac973.png)
--- bfs/bfs.cpp
+++ bfs/bfs.cpp
@@ -5,15 +5,19 @@
 #include <string.h>
 #include <cstddef>
 #include <omp.h>
+#include <unordered_map>
+#include <algorithm>
 
 #include "../common/CycleTimer.h"
 #include "../common/graph.h"
 
 #define ROOT_NODE_ID 0
 #define NOT_VISITED_MARKER -1
+#define NOT_INIT 0
 
 void vertex_set_clear(vertex_set* list) {
     list->count = 0;
+    memset(list->vertices,0,sizeof(int)*list->max_vertices);
 }
 
 void vertex_set_init(vertex_set* list, int count) {
@@ -57,24 +61,118 @@ void top_down_step(
                 int outgoing = g->outgoing_edges[neighbor];
                 if (distances[outgoing] == NOT_VISITED_MARKER) {
                     distances[outgoing] = distances[node] + 1;
-                    //#pragma omp atomic
-                    //int index; 
-                    //#pragma omp atomic
-                    int index=new_frontier->count;
-                    //#pragma omp critical
-                    //{
-                    #pragma omp atomic
-                    new_frontier->count++;
-                    //}
-                        //new_frontier->count++;
-                    
-                    //index = new_frontier->count++;
+                    /*
+                    int index; 
+                    #pragma omp critical
+                    index=new_frontier->count++;
                     new_frontier->vertices[index] = outgoing;
+                    */
+
+                    while(!__sync_bool_compare_and_swap(new_frontier->vertices+new_frontier->count++,NOT_INIT,outgoing));
+                    //#pragma omp atomic
+                    //new_frontier->count++;
+                    
                 }
             }
         //}
     }
 }
+
+/*
+void bottom_up_step(
+    Graph g,
+    vertex_set* frontier,
+    vertex_set* new_frontier,
+    vertex_set* unvisited_set,
+    vertex_set* new_unvisited_set,
+    int* distances)
+{
+    int mydistance=0;
+    #pragma omp parallel for
+    for (int i=0; i<unvisited_set->count; i++) {
+
+        int node = unvisited_set->vertices[i];
+
+        int start_edge = g->incoming_starts[node];
+        int end_edge = (node == g->num_nodes - 1)
+                           ? g->num_edges
+                           : g->incoming_starts[node + 1];
+        
+        bool into_new_frontier=false;
+        for(int neighbor=start_edge;neighbor<end_edge;neighbor++){
+            int incoming=g->incoming_edges[neighbor];
+            if(distances[incoming]!=NOT_VISITED_MARKER){
+                mydistance=distances[incoming]+1;
+                //#pragma omp critical
+                while(!__sync_bool_compare_and_swap(new_frontier->vertices+new_frontier->count++,NOT_INIT,node));
+                //new_frontier->vertices[new_frontier->count++]=node;
+                into_new_frontier=true;
+                break;
+            }
+        }
+        if(!into_new_frontier){
+            //while(!__sync_bool_compare_and_swap(new_unvisited_set->vertices+new_unvisited_set->count++,NOT_INIT,node));
+            //#pragma omp critical
+            new_unvisited_set->vertices[new_unvisited_set->count++]=node;
+        }
+    }
+    #pragma omp parallel for
+    for(int i=0;i<new_frontier->count;i++){
+        int node=new_frontier->vertices[i];
+        distances[node]=mydistance;
+    }
+}
+*/
+
+
+
+void bottom_up_step(
+    Graph g,
+    vertex_set* frontier,
+    vertex_set* new_frontier,
+    vertex_set* unvisited_set,
+    vertex_set* new_unvisited_set,
+    int* distances,
+    int lastdistance,
+    bool& newadd
+    )
+{
+    int mydistance=0;
+    #pragma omp parallel for
+    for (int i=0; i<unvisited_set->count; i++) {
+
+        int node = unvisited_set->vertices[i];
+        if(distances[node]!=NOT_VISITED_MARKER)continue;
+        int start_edge = g->incoming_starts[node];
+        int end_edge = (node == g->num_nodes - 1)
+                           ? g->num_edges
+                           : g->incoming_starts[node + 1];
+        
+        //bool into_new_frontier=false;
+        for(int neighbor=start_edge;neighbor<end_edge;neighbor++){
+            int incoming=g->incoming_edges[neighbor];
+            if(distances[incoming]==lastdistance){
+                
+                distances[node]=lastdistance+1;
+                newadd=true;
+                //#pragma omp critical
+                //while(!__sync_bool_compare_and_swap(new_frontier->vertices+new_frontier->count++,NOT_INIT,node));
+                //new_frontier->vertices[new_frontier->count++]=node;
+                //into_new_frontier=true;
+                break;
+            }
+        }
+    }
+    /*
+    #pragma omp parallel for
+    for(int i=0;i<new_frontier->count;i++){
+        int node=new_frontier->vertices[i];
+        distances[node]=mydistance;
+    }
+    */
+    
+}
+
 
 // Implements top-down BFS.
 //
@@ -133,6 +231,57 @@ void bfs_bottom_up(Graph graph, solution* sol)
     // As was done in the top-down case, you may wish to organize your
     // code by creating subroutine bottom_up_step() that is called in
     // each step of the BFS process.
+
+    std::unordered_map<int,int> dismap;
+    
+    vertex_set list1;
+    vertex_set list2;
+    vertex_set list3;
+    vertex_set list4;
+    vertex_set_init(&list1, graph->num_nodes);
+    vertex_set_init(&list2, graph->num_nodes);
+    vertex_set_init(&list3, graph->num_nodes);
+    vertex_set_init(&list4, graph->num_nodes);
+
+    vertex_set* frontier = &list1;
+    vertex_set* new_frontier = &list2;
+    vertex_set* unvisited_set=&list3;
+    vertex_set* new_unvisited_set=&list4;
+
+    // initialize all nodes to NOT_VISITED
+    for (int i=1; i<graph->num_nodes; i++){
+        sol->distances[i] = NOT_VISITED_MARKER;
+        unvisited_set->vertices[unvisited_set->count++]=i;
+    }
+
+    // setup frontier with the root node
+    frontier->vertices[frontier->count++] = ROOT_NODE_ID;
+    sol->distances[ROOT_NODE_ID] = 0;
+    int lastdistance=0;
+    bool newadd=true;
+    //while(frontier->count!=0 && unvisited_set->count!=0){
+    while(newadd){
+        vertex_set_clear(new_frontier);
+        vertex_set_clear(new_unvisited_set);
+        dismap.clear();
+        newadd=false;
+        bottom_up_step(graph, frontier, new_frontier, unvisited_set,new_unvisited_set,sol->distances,lastdistance,newadd);
+        // swap pointers
+        vertex_set* tmp = frontier;
+        frontier = new_frontier;
+        new_frontier = tmp;
+        lastdistance++;
+        /*
+        tmp=unvisited_set;
+        unvisited_set=new_unvisited_set;
+        new_unvisited_set=tmp;
+        */
+
+
+    }
+    
+    
+
 }
 
 void bfs_hybrid(Graph graph, solution* sol)
